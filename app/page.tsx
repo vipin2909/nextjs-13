ce640d3c76ab25897bcf7f31a586fb066e8f5768
Yash Rajput <yash.rajput@appinventiv.com>
2023-02-09 17:53:35 +0530

    Started implementing footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import '../styles/globals.css';
 import Link from 'next/link';
+import { Box } from '@mui/material';
 
 export default function Page() {
-  return <Link href="../about">Hello, Next.js!</Link>;
+  return <Link href="../about">Hello, Next.js!
+  <Box >
+    
+  </Box>
+  
+  </Link>;
 }
