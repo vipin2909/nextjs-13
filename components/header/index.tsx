dd82f3e38cd8ba469cb7e68618c5e36853430d3c
Ankur Kumar <ankur.kumar@appinventiv.com>
2023-02-13 13:27:31 +0530

    working on header

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -10,16 +10,18 @@ const Header = () => {
   const { headerData } = useHeader();
   return (
     <Box component="header" className="top-header" sx={styles.navContainer}>
-      <Box sx={styles.navContainerLeft}></Box>
-      <Box sx={styles.navContainerRight}>
-        <Box sx={styles.navHeader}>
-          {headerData.map((item) => {
-            return (
-              <Box key={item.id} sx={styles.headerItem}>
-                {item.name}
-              </Box>
-            );
-          })}
+      <Box className="container" sx={styles.navInnerContainer}>
+        <Box sx={styles.navContainerLeft}>Vie</Box>
+        <Box sx={styles.navContainerRight}>
+          <Box sx={styles.navHeader}>
+            {headerData.map((item) => {
+              return (
+                <Box key={item.id} sx={styles.headerItem}>
+                  {item.name}
+                </Box>
+              );
+            })}
+          </Box>
         </Box>
       </Box>
     </Box>
--- components/header/styles.ts
+++ components/header/styles.ts
@@ -1,14 +1,23 @@
+import { Colors } from "@utils/constants";
 import COLORS from "@utils/constants/colors";
 
 const styles = {
   navContainer: {
     width: "100%",
-    // background: "#000",
-    display: "flex",
+    padding: "1rem",
+    borderBottom: "1px solid rgba(255, 255, 255, 0.1)",
+    background: "transparent",
     position: "sticky",
+  },
+
+  navInnerContainer: {
+    display: "flex",
+    alignItems: "center",
   },
   navContainerLeft: {
     width: "50%",
+    color: Colors.vieWhite,
+    textAlign: "left",
   },
   navContainerRight: {
     width: "50%",
